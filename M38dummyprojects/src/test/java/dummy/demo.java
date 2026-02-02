e38a2d256f349f25c4dbaaad35732ea9ce1731a0
USER <USER@DESKTOP-VHIP1A7>
2026-02-02 17:19:28 +0530

    commit

---
+++ M38dummyprojects/src/test/java/dummy/demo.java
@@ -0,0 +1,13 @@
+package dummy;
+
+public class demo 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hello everyone");
+		System.out.println("hello everyone");
+		System.out.println("hello everyone");
+		System.out.println("hello everyone");
+		System.out.println("hello everyone");
+	}
+}
